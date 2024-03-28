c271764c6d3e1c9a628852720511a7f11398c9b7
alaisgomes <alaisgomes@gmail.com>
2024-03-28 13:18:18 -0400

    Publishing some random modules

--- modules/django-todo-list/django_todo_list/todo_list/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Task, TaskList
-
-admin.site.register(Task)
-admin.site.register(TaskList)
--- modules/django-todo-list/django_todo_list/todo_list/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TodolistConfig(AppConfig):
-    name = 'todo_list'
